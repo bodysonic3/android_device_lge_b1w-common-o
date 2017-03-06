117646d403bbbd8ba94d0eaf659d2e3e8238419e
covasic <covasic96@gmail.com>
2017-03-06 01:32:34 +0100

    B1w-camera: add missing camera parms
    Change-Id: I51e3fbe26f1a9ba2d52cf46dd3c64e0a484ac51f

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -21,12 +21,14 @@ const char CameraParameters::BEAUTY_SHOT_OFF[] = "beauty-shot"; \
 const char CameraParameters::BEAUTY_SHOT_ON[] = "beauty-shot"; \
 const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot"; \
 const char CameraParameters::BURST_SHOT_ON[] = "burst-shot"; \
+const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual-position"; \
 const char CameraParameters::KEY_AUDIO_ZOOM[] = "audio-zoom"; \
 const char CameraParameters::KEY_AUDIO_ZOOM_SUPPORTED[] = "audio-zoom-supported"; \
 const char CameraParameters::KEY_BEAUTY_SHOT[] = "beauty-shot"; \
 const char CameraParameters::KEY_BEAUTY_SHOT_SUPPORTED[] = "beauty-shot-supported"; \
 const char CameraParameters::KEY_BURST_SHOT[] = "burst-shot"; \
 const char CameraParameters::KEY_BURST_SHOT_SUPPORTED[] = "burst-shot-supported"; \
+const char CameraParameters::KEY_FOCUS_MODE_MANUAL_POSITION[] = "manual-position"; \
 const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING[] = "object-tracking"; \
 const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[] = "object-tracking-supported"; \
 const char CameraParameters::KEY_ISO_MODE[] = "iso"; \
@@ -37,6 +39,7 @@ const char CameraParameters::KEY_VIDEO_WDR[] = "video-wdr"; \
 const char CameraParameters::KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported"; \
 const char CameraParameters::VIDEO_WDR_OFF[] = "video-wdr"; \
 const char CameraParameters::VIDEO_WDR_ON[] = "video-wdr"; \
+const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "white-balance-manual"; \
 const char CameraParameters::OBJECT_TRACKING_ON[] = "object-tracking"; \
 const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking"; \
 
@@ -47,12 +50,14 @@ const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking"; \
     static const char BEAUTY_SHOT_ON[]; \
     static const char BURST_SHOT_OFF[]; \
     static const char BURST_SHOT_ON[]; \
+    static const char FOCUS_MODE_MANUAL_POSITION[]; \
     static const char KEY_AUDIO_ZOOM[]; \
     static const char KEY_AUDIO_ZOOM_SUPPORTED[]; \
     static const char KEY_BEAUTY_SHOT[]; \
     static const char KEY_BEAUTY_SHOT_SUPPORTED[]; \
     static const char KEY_BURST_SHOT[]; \
     static const char KEY_BURST_SHOT_SUPPORTED[]; \
+    static const char KEY_FOCUS_MODE_MANUAL_POSITION[]; \
     static const char KEY_FOCUS_MODE_OBJECT_TRACKING[]; \
     static const char KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[]; \
     static const char KEY_ISO_MODE[]; \
@@ -63,5 +68,6 @@ const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking"; \
     static const char KEY_VIDEO_WDR_SUPPORTED[]; \
     static const char VIDEO_WDR_OFF[]; \
     static const char VIDEO_WDR_ON[]; \
+    static const char WHITE_BALANCE_MANUAL_CCT[]; \
     static const char OBJECT_TRACKING_ON[]; \
     static const char OBJECT_TRACKING_OFF[]; \
