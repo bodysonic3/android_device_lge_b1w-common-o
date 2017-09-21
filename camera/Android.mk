7379978b8884e407715f57e5c104ef561fbf8995
bodysonic3 <bodysonic3@163.com>
2017-09-21 14:16:22 +0800

    add missing libs for headers

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,9 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := CameraWrapper.cpp
-LOCAL_SHARED_LIBRARIES := libhardware liblog libcamera_client libutils android.hidl.token@1.0-utils libgui
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
 LOCAL_STATIC_LIBRARIES := libarect libbase
-LOCAL_C_INCLUDES += system/media/camera/include
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libgui\
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+	
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
